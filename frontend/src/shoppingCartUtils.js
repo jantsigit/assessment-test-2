36db3bb9d4b58e5d3067904869346afc1bfdfd41
jantsigit <jan.lohva@gmail.com>
2022-10-07 10:44:26 +0300

    funktiot korjattu

--- frontend/src/shoppingCartUtils.js
+++ frontend/src/shoppingCartUtils.js
@@ -1,15 +1,26 @@
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart total.
-function calculateTotal (items) {
-  return 9999;
+function calculateTotal(items) {
+  let yhteensä = 0
+  const priceTaulukko = items.map((a) => a.price)
+  for (const summa of priceTaulukko) {
+    yhteensä += summa
+  }
+  return yhteensä
 }
 
 // Takes a list of fruit objects as arguments
 // eg. [{'name': 'Banana', 'price': 1}, {'name': 'Apple', 'price': 2}]
 // Should return the cart average.
-function calculateAvg (items) {
-  return 9999;
+function calculateAvg(items) {
+  let yhteensä = 0
+  const priceTaulukko = items.map((a) => a.price)
+  for (const summa of priceTaulukko) {
+    yhteensä += summa
+  }
+  const keskiarvo = yhteensä / priceTaulukko.length
+  return keskiarvo
 }
 
-export { calculateTotal, calculateAvg };
+export { calculateTotal, calculateAvg }
